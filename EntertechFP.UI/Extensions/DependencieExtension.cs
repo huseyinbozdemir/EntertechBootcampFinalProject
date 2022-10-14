4778651f6ca00cfe4ed3f345a88f0a9d68c54330
Huseyin Bozdemir <huseinbozdemir@gmail.com>
2022-10-14 16:31:12 +0300

    UI Dependencies added

---
+++ EntertechFP.UI/Extensions/DependencieExtension.cs
@@ -0,0 +1,14 @@
+﻿using EntertechFP.UI.Utils;
+
+namespace EntertechFP.UI.Extensions
+{
+    public static class DependencieExtension
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddScoped<CookieHelper>();
+            services.AddScoped(x => new RequestHelper(configuration.GetSection("ApiKey").Value));
+            return services;
+        }
+    }
+}
--- EntertechFP.UI/Program.cs
+++ EntertechFP.UI/Program.cs
@@ -1,34 +1,14 @@
-using EntertechFP.UI.Utils;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using EntertechFP.UI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(options => options.DefaultScheme = "admin_scheme")
-    .AddCookie("admin_scheme", options =>
-    {
-        options.LoginPath = "/login";
-        options.Cookie.Name = "admin_session";
-        options.AccessDeniedPath = "/login";
-        options.LogoutPath = "/login";
-
-    });
-builder.Services.AddAuthentication(options => options.DefaultScheme = "user_scheme")
-    .AddCookie("user_scheme", options =>
-    {
-        options.LoginPath = "/login";
-        options.Cookie.Name = "user_session";
-        options.AccessDeniedPath = "/login";
-        options.LogoutPath = "/login";
-
-    });
-builder.Services.AddScoped<CookieHelper>();
-builder.Services.AddScoped(x => new RequestHelper(builder.Configuration.GetSection("ApiKey").Value));
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddCookies()
+    .AddDependencies(builder.Configuration)
+    .AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
